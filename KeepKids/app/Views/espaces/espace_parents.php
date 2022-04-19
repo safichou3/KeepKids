c33d42e721395bc90de23fd26d71f300cd32360d
safia <safia.medjahed@gmail.com>
2022-04-19 20:55:57 +0200

    add img

--- KeepKids/app/Views/espaces/espace_parents.php
+++ KeepKids/app/Views/espaces/espace_parents.php
@@ -1,1 +1,13 @@
-espace parent
+<?= $this->extend('master') ?>
+
+<?= $this->section('title') ?>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+<div class="body-parents">
+  <h1 class="parents-title">Mon espace</h1>
+</div>
+
+<?= $this->endSection() ?>
