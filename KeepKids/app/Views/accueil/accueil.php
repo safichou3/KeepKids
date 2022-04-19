efcf33c8835e1805f1f36cce25f462a94ab3c628
Hyotre1 <72597664+Hyotre1@users.noreply.github.com>
2022-04-19 17:53:05 +0200

    index

--- KeepKids/app/Views/accueil/accueil.php
+++ KeepKids/app/Views/accueil/accueil.php
@@ -2,10 +2,34 @@
 
 <?= $this->section('title') ?>
     Bienvenue sur la page d'accueil sur notre site
+
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-   
-<h2>Contenu du site</h2>
+
+<div class="section2">
+<section>
+<em>About Us</em>
+<h1 class="title">Nous sommes une jeune équipe qui mettons nos qualité a l'oeuvre pour votre confort</h1>
+<p>En imaginant ce site nous avons pensé aux jeunes parents dans le besoin qui ne savent pas ou ils peuvent faire garder leurs bout-de-choux. </p>
+<p>Pour cela nous avons mis en commun les compétences de 6 jeunes développeurs.</p>
+</section>
+<section>
+<img src="https://www.unicef.org/sites/default/files/styles/hero_mobile/public/UN0318726_0.jpg?itok=Gr_K1zyW" alt="freepik.com" loading="lazy">
+</section>
+</div>
+<div class="section3">
+<section>
+<img src="https://www.formation-relation-aide.com/wp-content/uploads/2019/02/formation_relation_aide_enfants_cours-600x400.jpg" alt="freepik.com" loading="lazy">
+</section>
+<section>
+<em>Why Us</em>
+<h1 class="title">Notre plateforme est sûr et vous permettra d'aller au travail en toute sérenité</h1>
+<p>En effet, chacun des professionels pouvant garder des enfants nous aura donner une preuve de sa capacité a garder des enfants.</p>
+<p>Le site vous indiqueras oû sont les professionels les plus proches afin de vous aider le mieux possible </p>
+</section>
+</div>
+
+
 
 <?= $this->endSection() ?>
--- KeepKids/app/Views/templates/header.php
+++ KeepKids/app/Views/templates/header.php
@@ -1,8 +1,14 @@
-<ul>
-    <li><a href="<?= base_url(); ?>">Logo</a></li>
-    <li><a href="<?= base_url(); ?>">Accueil</a></li>
-    <li><a href="<?= base_url(); ?>/news/create">A propos</a></li>
-    <li><a href="<?= base_url(); ?>/categories">Mentions Legales</a></li>
-    <li><a href="<?= base_url(); ?>/categories">Connexion</a></li>
-
-</ul>
+<div class="header">
+    <div class="nav-menu">
+        <div class="logo">KEEPKIDS</div>
+        <div class="menu">
+            <ul>
+                <li><a href="<?= base_url(); ?>/news/create">Logo</a></li>
+                <li><a href="<?= base_url(); ?>/news/create">Accueil</a></li>
+                <li><a href="<?= base_url(); ?>/news/create">A propos</a></li>
+                <li><a href="<?= base_url(); ?>/categories">Mentions Legales</a></li>
+                <li><a href="<?= base_url(); ?>/categories">Connexion</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
