cfc7fa2041dcc1a30b031bdcfce78ecd33a00904
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-04-19 19:20:15 +0200

    images accueil

---
+++ KeepKids/app/Views/templates/footer.php
@@ -0,0 +1,46 @@
+<footer id="footer-2-cols" class="site-footer">
+    <div id="footer-container">
+        <div id="footer-grid">
+            <div id="left-footer-section" class="footer-section">
+                <div id="footer-search">
+                    <form action="">
+                        <input type="search" placeholder="Rechercher sur le site"><button type="submit">Search</button>
+                    </form>
+                </div>
+                <div class="footer-information">
+                    <p><img src="https://img.icons8.com/ios-filled/12/999999/marker.png">15 rue de la sauce, Rouen</p>
+                    <p><img src="https://img.icons8.com/ios-filled/12/999999/phone.png">02 56 94 15 22</p>
+                    <p><img src="https://img.icons8.com/ios-filled/12/999999/mail.png">contact@keepkids.fr</p>
+                    <p><img src="https://img.icons8.com/ios-filled/12/999999/clock.png">9H00 - 17H30</p>
+                </div>
+            </div>
+            <div id="right-footer-section" class="footer-section">
+                <div class="footer-links">
+                    <ul>
+                        <li role="menuitem"><a href="#">Accueil</a></li>
+                        <li role="menuitem"><a href="#">A propos</a></li>
+                        <li role="menuitem"><a href="#">Nous contacter</a></li>
+                        <li role="menuitem"><a href="#">Mentions légales</a></li>
+                        <li role="menuitem"><a href="#">Se connecter</a></li>
+                        <li role="menuitem"><a href="#">S'inscrire</a></li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div id="copyright-information">
+            <div id="footer-logo-section">
+                <div id="footer-logo">
+                    <img src="https://raw.githubusercontent.com/taviskaron/2-3-4-column-footers/main/img/logo.png" alt="">
+                </div>
+            </div>
+            <div id="copyright-text">
+                &copy; KeepKids Inc. 2022. Tout les droits reservés.
+            </div>
+            <div id="social-buttons">
+                <img src="https://img.icons8.com/ios-filled/25/999999/facebook--v1.png" /><img src="https://img.icons8.com/ios-filled/25/999999/telegram-app.png" /><img src="https://img.icons8.com/ios-filled/25/999999/pinterest--v1.png" /><img src="https://img.icons8.com/ios-filled/25/999999/instagram--v1.png" />
+            </div>
+        </div>
+    </div>
+</footer>
