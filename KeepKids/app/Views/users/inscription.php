2ab83f347a9bd9c52e951ef588229a8883a28347
Hyotre1 <72597664+Hyotre1@users.noreply.github.com>
2022-04-20 15:02:15 +0200

    commit

--- KeepKids/app/Views/users/inscription.php
+++ KeepKids/app/Views/users/inscription.php
@@ -9,19 +9,21 @@ Création d'un compte parent
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-
+<div class="body-inscription">
 <form action="" method="POST">
     <label>Prénom</label><br>
-    <input type="text" id="prenom" name="username"><br>
+    <input class="input-inscription" type="text" id="prenom" name="username" placeholder="Prénom"><br>
     <label>Nom</label><br>
-    <input type="text" id="nom" name="username"><br>
+    <input class="input-inscription" type="text" id="nom" name="username" placeholder="Nom"><br>
     <label>Téléphone</label><br>
-    <input type="tel" id="tel" name="phone" required><br>
-    <label>Mot de passe</label><br>
-    <input type="password" id="password" name="password"><br>
+    <input class="input-inscription" type="tel" id="tel" name="phone" placeholder="tel" required><br>
+    <label>Mot de passe</label> <br>
+    <input class="input-inscription" type="password" id="password" name="password" placeholder="mot de passe"><br>
     <label>Email</label><br>
-    <input type="email" id="email" required><br>
-    <input name="title" type="text" class="form-control" id="adresse" placeholder="adresse"><br>
-    <input type="submit" value="inscription">
+    <input class="input-inscription" type="email" id="email" placeholder="email" required><br>
+    <label>adresse</label><br>
+    <input class="input-inscription" name="title" type="text" class="form-control" id="adresse" placeholder="adresse"><br>
+    <input class="inscription" type="submit" value="Inscription">
 </form>
+</div>
 <?= $this->endSection() ?>
