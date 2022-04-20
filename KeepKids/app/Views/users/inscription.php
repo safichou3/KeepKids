c8d15774bd8570eb7ec9bc23e7d17327ddd5cc72
Hyotre1 <72597664+Hyotre1@users.noreply.github.com>
2022-04-20 12:18:28 +0200

    imput

--- KeepKids/app/Views/users/inscription.php
+++ KeepKids/app/Views/users/inscription.php
@@ -1,49 +1,22 @@
 <?= $this->extend('master') ?>
 
 <?= $this->section('css') ?>
-<link href="<?= base_url(); ?>/css/users.css" rel="stylesheet">
+<link href="<?= base_url(); ?>/css/newsCreation.css" rel="stylesheet">
 <?= $this->endSection() ?>
 
 <?= $this->section('title') ?>
-Création d'une news
+Création d'un compte parent
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-<form method="POST" action="<?= base_url(); ?>/news/create" enctype="multipart/form-data">
-    <div class="form-group">
-        <label for="title">nom</label>
-        <input name="title" type="text" class="form-control" id="name" placeholder="nom">
-    </div>
-    <div class="form-group">
-        <label for="title">prenom</label>
-        <input name="title" type="text" class="form-control" id="firstname" placeholder="prenom">
-    </div>
 
-    <div>
-        <label for="email">entrer votre email:</label>
-        <input type="email" id="email" pattern=".+@mail\.com" size="30" required>
-    </div>
-    <div>
-        <label for="phone">entrer votre numéro de tel:</label>
+    <form action="" method="POST" > 
+    <input type="text" id="prenom" name="username">
+    <input type="text" id="nom" name="username">
+    <input type="tel" id="tel" name="phone" required>
+    <input type="password" id="password" name="password">
+    <input type="email" id="email" pattern=".+@mail\.com" size="30" required>
+    <input name="title" type="text" class="form-control" id="adresse" placeholder="adresse">
+    </form>
 
-        <input type="tel" id="phone" name="phone" required>
-    </div>
-    <div class="form-group">
-        <input type="file" name="newsImage" size="20" />
-    </div>
-    <div>
-        <label for="pet-select">Choose a pet:</label>
-
-        <select name="pets" id="pet-select">
-            <option value="">--Please choose an option--</option>
-            <option value="dog">Dog</option>
-            <option value="cat">Cat</option>
-            <option value="hamster">Hamster</option>
-            <option value="parrot">Parrot</option>
-            <option value="spider">Spider</option>
-            <option value="goldfish">Goldfish</option>
-        </select>
-    </div>
-    <button type="submit" class="btn btn-primary">valider</button>
-</form>
 <?= $this->endSection() ?>
