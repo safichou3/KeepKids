a349dce8a93ccbad0dd29cbb76058cc49609073e
Hyotre1 <72597664+Hyotre1@users.noreply.github.com>
2022-04-25 11:01:49 +0200

    Update main.js

--- KeepKids/public/js/main.js
+++ KeepKids/public/js/main.js
@@ -1,13 +1,10 @@
 
 const password = document.getElementById("passwordInput");
-const siret = document.getElementById("siretImput");
 const passwordErrorSize = document.getElementById("passwordErrorSize");
 const passwordErrorSpecial = document.getElementById("passwordErrorSpecial");
 const passwordErrorNombre = document.getElementById("passwordErrorNombre");
 const passwordErrorMinuscule = document.getElementById(	"passwordErrorMinuscule");
 const passwordErrorMajuscule = document.getElementById(	"passwordErrorMajuscule");
-const onlyText = document.getElementById("onlyText");
-const siretERROR = document.getElementById("siretERROR");
 password.addEventListener("input", passwordChange);
 function passwordChange() {
 	
@@ -45,45 +42,4 @@ function passwordChange() {
 	} else {
 		passwordErrorSpecial.style.color = "red";
 	}
-}
-nom.addEventListener("oninput",onlyText)
-function onlyText(){
-if (nom.value.match(/[0-9]/, 'g')) {
-	onlyText.style.color = "red";
-} else {
-	onlyText.style.color = "green";
-}
-}
-siret.addEventListener("siret", passwordChange);
-function SIRET(){
-if (siret.value.length =14 ) {
-	siretERROR.style.color = "green";
-} else {
-	siretERROR.style.color = "red";
-}
-}
-
-
-
-
-// REGEX
-
-function hasEmailValid(str) {
-    return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str));
-}
-
-function hasLowerCase(str) {
-    return (/[a-z]/.test(str));
-}
-
-function hasUpperCase(str) {
-    return (/[A-Z]/.test(str));
-}
-
-function hasNumber(str) {
-    return (/[0-9]/.test(str));
-}
-
-function hasSpecial(str) {
-    return (/[!-/]|[:-@]|[[-`]|[{-~]/.test(str));
 }
