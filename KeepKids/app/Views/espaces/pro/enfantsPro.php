ae94fc69f07d3369282dfbb65657c6087d3be8a6
Ousbane Houda <82388160+ousbahou@users.noreply.github.com>
2022-05-05 12:25:38 +0200

    datatable

--- KeepKids/app/Views/espaces/pro/enfantsPro.php
+++ KeepKids/app/Views/espaces/pro/enfantsPro.php
@@ -55,5 +55,6 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('js') ?>
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script>
 <script src="<?= base_url(); ?>/js/main.js"></script>
 <?= $this->endSection() ?>
