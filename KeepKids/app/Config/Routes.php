0e12f27403b7a4205230affb45580d602c5d78a3
safia <safia.medjahed@gmail.com>
2022-05-06 15:06:08 +0200

    Documents projet

--- KeepKids/app/Config/Routes.php
+++ KeepKids/app/Config/Routes.php
@@ -20,9 +20,9 @@ $routes->setDefaultNamespace('App\Controllers');
 $routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
-// $routes->set404Override(function () {
-//     echo view('404.php');
-// });
+$routes->set404Override(function () {
+    echo view('404.php');
+});
 $routes->setAutoRoute(true);
 
 /*
